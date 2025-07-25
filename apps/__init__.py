3d88d3ad5d23c8cec769144fd252e8aaa053aa7f
yoonkm2726 <yoonkm2726@gmail.com>
2025-07-25 22:58:35 +0900

    Delete apps directory

--- apps/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Commax Wallpad Addon apps package
-""" 
--- apps/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/with-contenv bashio
-export MQTT_HOST=$(bashio::services mqtt "host")
-export MQTT_USER=$(bashio::services mqtt "username")
-export MQTT_PASSWORD=$(bashio::services mqtt "password")
-python3 -m apps.main
